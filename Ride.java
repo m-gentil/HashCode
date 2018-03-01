becc4f75f2463ae2436b9ac717b042ca04fa29a6
Mattia Gentil <mattygent94@gmail.com>
2018-03-01 20:14:26 +0100

    third commit

--- Ride.java
+++ Ride.java
@@ -9,6 +9,54 @@ public class Ride {
 		early = e;
 		late = l;
 	}
+
+	public int getStartRow() {
+		return startRow;
+	}
+
+	public void setStartRow(int startRow) {
+		this.startRow = startRow;
+	}
+
+	public int getStartCol() {
+		return startCol;
+	}
+
+	public void setStartCol(int startCol) {
+		this.startCol = startCol;
+	}
+
+	public int getEndRow() {
+		return endRow;
+	}
+
+	public void setEndRow(int endRow) {
+		this.endRow = endRow;
+	}
+
+	public int getEndCol() {
+		return endCol;
+	}
+
+	public void setEndCol(int endCol) {
+		this.endCol = endCol;
+	}
+
+	public int getEarly() {
+		return early;
+	}
+
+	public void setEarly(int early) {
+		this.early = early;
+	}
+
+	public int getLate() {
+		return late;
+	}
+
+	public void setLate(int late) {
+		this.late = late;
+	}
 	
 	
 }
--- Vehicle.java
+++ Vehicle.java
@@ -1,4 +1,65 @@
 public class Vehicle {
-	
-	
+	int currRow, currCol;
+	Ride ride;
+	boolean hasPassenger;
+
+	public Vehicle() {
+		currRow = 0;
+		currCol = 0;
+		ride = null;
+		hasPassenger = false;
+	}
+
+	public void update(int t) {
+		if (ride == null) {
+			pickNewRide();
+		} else if (hasPassenger) {
+			int destRow = ride.getEndRow();
+
+			if (currRow > destRow) {
+				currRow--;
+			} else if (currRow < destRow) {
+				currRow++;
+			} else {
+				int destCol = ride.getEndCol();
+
+				if (currCol > destCol) {
+					currCol--;
+				} else {
+					currCol++;
+				}
+
+				if (currCol == destCol) {
+					hasPassenger = false;
+					// Scrivi su file
+					pickNewRide();
+				}
+			}
+		} else {
+			int destRow = ride.getStartRow();
+
+			if (currRow > destRow) {
+				currRow--;
+			} else if (currRow < destRow) {
+				currRow++;
+			} else {
+				int destCol = ride.getStartCol();
+
+				if (currCol > destCol) {
+					currCol--;
+				} else {
+					currCol++;
+				}
+
+				if (currCol == destCol && ride.getEarly() <= t) {
+					// Scrivi su file
+					hasPassenger = true;
+				}
+			}
+		}
+	}
+
+	public void pickNewRide() {
+
+	}
 }
