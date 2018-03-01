53d8668dae9f4bfa65502ebeb443edf6ef9d18cd
Mattia Gentil <mattygent94@gmail.com>
2018-03-01 20:11:22 +0100

    second commit

---
+++ Ride.java
@@ -0,0 +1,14 @@
+public class Ride {
+	int startRow, startCol, endRow, endCol, early, late;
+	
+	public Ride(int sR, int sC, int eR, int eC, int e, int l) {
+		startRow = sR;
+		startCol = sC;
+		endRow = eR;
+		endCol = eC;
+		early = e;
+		late = l;
+	}
+	
+	
+}
